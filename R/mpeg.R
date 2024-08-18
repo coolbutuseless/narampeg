e1c2892175aeba0ffb7378b6204866c71bc8bf4e
mike <mikefc@coolbutuseless.com>
2024-08-18 14:41:33 +1000

    syncrhonised playback is possible

--- R/mpeg.R
+++ R/mpeg.R
@@ -4,8 +4,8 @@
 #' Initialise mpeg playback
 #'
 #' @param file mpeg1 filename
-#' @param video logical. Should video be decoded?  Default: FALSE
-#' @param audio logical. Should audio be decoded?  Default: FALSE
+#' @param video logical. Should video be decoded?  Default: TRUE
+#' @param audio logical. Should audio be decoded?  Default: TRUE
 #' @param audio_stream audio stream to play. Default: 0
 #' 
 #' @return mpeg decoding context
@@ -14,7 +14,7 @@
 init_mpeg <- function(
     file, 
     video = TRUE,
-    audio = FALSE,
+    audio = TRUE,
     audio_stream = 0) {
   file <- normalizePath(file, mustWork = TRUE)
   .Call(init_mpeg_, file, isTRUE(video), isTRUE(audio), as.integer(audio_stream))
@@ -99,7 +99,22 @@ mpeg_rewind <- function(ctx) {
 }
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Samples per channel per frame
+#' @export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+samples_per_channel_per_frame <- 1152
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Time to play a single frame of audio at 44100 kHz
+#' @export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+audio_time_per_frame <- 1152 / 44100
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Video playback
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if (FALSE) {
   
   library(grid)
@@ -109,7 +124,8 @@ if (FALSE) {
   x11(type = 'dbcairo', antialias = 'none', width = 7, height = 4)
   dev.control(displaylist = 'inhibit')
   
-  ctx <- narampeg::init_mpeg()
+  file <- "~/projectsdata/mpeg/bjork-all-is-full-of-love.mpg"
+  ctx <- narampeg::init_mpeg(file)
   
   mpeg_seek(ctx, 120)
   
@@ -124,29 +140,103 @@ if (FALSE) {
     gov_wait(gov)
   }      
   
-  N <- 100
-  snds <- vector('list', N)
-  for (i in seq_len(N)) {
-    snds[[i]] <- mpeg_decode_audio(ctx)
-  }
-  
-  snd <- unlist(snds)
+  snd <- mpeg_decode_audio(ctx, 100)
   snd <- matrix(snd, nrow = 2)
   snd <- audio::as.audioSample(snd)
   audio::play(snd)  
 }
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Audio playback
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if (FALSE) {
   
+  library(rminiaudio)  
   library(narampeg)
+  library(governor)
   
-  ctx <- narampeg::init_mpeg(); mpeg_info(ctx)
+  nframes    <- 100
+  frame_time <- 1152 / 44100
+  segment_time <- nframes * frame_time
   
+  stream <- rminiaudio::open_stream('write', rate = 44100, channels = 2)
   
+  file <- "~/projectsdata/mpeg/bjork-all-is-full-of-love.mpg"
+  ctx <- narampeg::init_mpeg(file, video = TRUE, audio = TRUE)
+  
+  start <- Sys.time()
+  
+  count <- 1L
+  while(TRUE) {
+    cat(count, " ")
+    snd <- mpeg_decode_audio(ctx, nframes)
+    
+    if (is.null(snd)) {
+      message("Done")
+      break;
+    }
+    
+    wrote_audio <- rminiaudio::stream_write(stream, snd, rate = 44100, channels = 2, wait = FALSE)
+    while (!wrote_audio) {
+      Sys.sleep(0.2)
+      wrote_audio <- rminiaudio::stream_write(stream, snd, rate = 44100, channels = 2, wait = FALSE)
+    }
+    
+    count <- count + 1L
+  }      
+  
+  rminiaudio::close_stream(stream)
+  Sys.time() - start
 }
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# AUDIO + Video playback
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if (FALSE) {
+  
+  library(grid)
+  library(governor)
+  library(rminiaudio)
+  library(narampeg)
+  
+  x11(type = 'dbcairo', antialias = 'none', width = 7, height = 4)
+  dev.control(displaylist = 'inhibit')
+  
+  n_audio_frames     <- 100
+  audio_frame_time   <- 1152 / 44100
+  audio_segment_time <- n_audio_frames * audio_frame_time
+  stream <- rminiaudio::open_stream('write', rate = 44100, channels = 2)
+  
+  file <- "~/projectsdata/mpeg/bjork-all-is-full-of-love.mpg"
+  ctx <- narampeg::init_mpeg(file)
+  
+  gov <- governor::gov_init(1/25)  # 25fps
+  tim <- governor::timer_init(audio_segment_time - 0.2)
+  
+  snd <- mpeg_decode_audio(ctx, n_audio_frames)
+  rminiaudio::stream_write(stream, snd, rate = 44100, channels = 2, wait = FALSE)
+  
+  audio_to_inject <- FALSE
+  
+  while(TRUE) {
+    nr <- mpeg_decode_video(ctx)
+    if (is.null(nr)) break;
+    dev.hold(); grid.raster(nr); dev.flush()
+    if (governor::timer_check(tim)) {
+      snd <- mpeg_decode_audio(ctx, nframes)
+      audio_to_inject <- !rminiaudio::stream_write(stream, snd, rate = 44100, channels = 2, wait = FALSE)
+    }
+    if (audio_to_inject) {
+      audio_to_inject <- !rminiaudio::stream_write(stream, snd, rate = 44100, channels = 2, wait = FALSE)
+    }
+    gov_wait(gov)
+  }
+  
+  rminiaudio::close_stream(stream)
+  
+}
 
 
 
